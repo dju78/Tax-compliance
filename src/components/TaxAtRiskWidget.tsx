fd30efdd3300a6d9ae62c4fc997b88ddec1c9c16
dju78 <1978dju@gmail.com>
2026-01-13 20:44:02 +0000

    fix: Tax at Risk widget links to Smart Ledger for transaction categorization

--- src/components/TaxAtRiskWidget.tsx
+++ src/components/TaxAtRiskWidget.tsx
@@ -136,7 +136,7 @@ export function TaxAtRiskWidget({ companyId }: { companyId: string }) {
 
             {/* Action Button */}
             <Link
-                to="/expense_audit"
+                to="/smart_ledger"
                 style={{
                     display: 'inline-block',
                     marginTop: '1.5rem',
